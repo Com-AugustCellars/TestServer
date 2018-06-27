b29f140201f397d9ffd6de12f53db9b56b9fe8cb
Jim Schaad <ietf@augustcellars.com>
2018-06-27 08:07:17 +0200

    Updates for more CoapCore tests to be implemented

---
+++ TestServer/InteropTests/CoapCoreTests/LocationQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.AugustCellars.CoAP;
+using Com.AugustCellars.CoAP.Server.Resources;
+
+namespace TestServer.InteropTests.CoapCoreTests
+{
+    class LocationQuery : Resource
+    {
+        public LocationQuery() : base("location_query")
+        {
+
+        }
+
+        protected override void DoPost(CoapExchange exchange)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("POST to /location_query w/ options {0}", exchange.Request.UriQueries);
+
+            exchange.Respond(StatusCode.Changed, sb.ToString(), MediaType.TextPlain);
+        }
+    }
+}
---
+++ TestServer/InteropTests/CoapCoreTests/MultiFormat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.AugustCellars.CoAP;
+using Com.AugustCellars.CoAP.Server.Resources;
+
+namespace TestServer.InteropTests.CoapCoreTests
+{
+    class MultiFormat : Resource
+    {
+        public MultiFormat() : base("multi-format")
+        {
+
+        }
+
+        protected override void DoGet(CoapExchange exchange)
+        {
+            foreach (Option i in  exchange.Request.GetOptions(OptionType.Accept)) {
+                switch (i.IntValue) {
+                    case MediaType.TextPlain:
+                        exchange.Respond(StatusCode.Content, "You asked for plain text", MediaType.TextPlain);
+                        return;
+
+                    case MediaType.ApplicationXml:
+                        exchange.Respond(StatusCode.Content, "<t>You asked <em> for XML</em></t>",
+                                         MediaType.ApplicationXml);
+                        return;
+                }
+            }
+
+            exchange.Respond(StatusCode.BadOption);
+        }
+    }
+}
